63fbbf95dc9cdcdbc8dcfbe9318ec68b3b382bba
ssyangy <ssyangy@gmail.com>
2012-10-19 13:43:40 +0800

    create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P2ex::Application.config.secret_token = '5ea08ebe48456cb12b4843b0bbb344a68a8aba9f87d3b99bcb02355f3b31eefd3c2e8ebd3d0c253f6e3809e50ecea3e524d805fc62a9ddb2e7e377988b7b6a48'
